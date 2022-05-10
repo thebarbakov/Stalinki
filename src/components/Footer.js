7be6d2197fbc969cf47900d55817708b5b21fb61
thebarbakov <94574341+thebarbakov@users.noreply.github.com>
2022-05-10 04:39:46 +0300

    inst

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 function Footer() {
     return (
       <footer className="footer">
-          <a href='https://vk.com/thebarbakov' className="footer__text">
+          <a href='https://www.instagram.com/thebarbakov' className="footer__text">
             © {new Date().getFullYear()} Yaroslav Barbakov
           </a>
           <a href='https://vk.com/jaroslava2004' className="footer__text">
