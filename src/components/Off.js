b30d34eed1a5d552803e93137533f77cd1eee380
thebarbakov <94574341+thebarbakov@users.noreply.github.com>
2022-05-12 18:34:36 +0300

    upd turned Off

--- src/components/Off.js
+++ src/components/Off.js
@@ -1,12 +1,23 @@
 import React from "react"
 import logo from '../images/ui/logo.png'
+import App from './App'
 
 function Off() {
+    const [clicks, setClick] = React.useState(0)
+
+    const onImgCLick = (e) => {
+        setClick(clicks+1)
+    }
+
+
+
     return (
-      <div className="off">
-          <img className='off__image' src={logo} alt='Лого' />
-          <h1 className="off__text">App is sleeping now.</h1>
-      </div>
+          clicks === 17
+          ? <App />
+          : <div className="off">
+            <img className='off__image' src={logo} alt='Лого' onClick={onImgCLick} />
+            <h1 className="off__text">App is sleeping now.</h1>
+            </div>
     );
 }
 
