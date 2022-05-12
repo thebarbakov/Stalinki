ae067dfe6c3043b22fde5457d55e70f9e0b918cf
thebarbakov <94574341+thebarbakov@users.noreply.github.com>
2022-05-12 21:36:08 +0300

    off is off with import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './components/App'
 import './blocks/App.scss'
 // import App from './components/App';
 
