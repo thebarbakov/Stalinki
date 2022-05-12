338028b33f361d281e8758c20392466f0c134d15
thebarbakov <94574341+thebarbakov@users.noreply.github.com>
2022-05-12 21:30:03 +0300

    off is off

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Off from './components/Off'
 import './blocks/App.scss'
 // import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Off />
+    <App />
   </React.StrictMode>
 );
